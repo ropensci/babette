6da3816423898822d7c4221aabb57cbe85e12338
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-30 12:19:03 +0100

    Fixed tests

--- tests/testthat/test-get_path.R
+++ tests/testthat/test-get_path.R
@@ -4,12 +4,12 @@ test_that("use", {
 
   testthat::expect_equal(
     system.file("extdata", "anthus_nd2.fas", package = "babette"),
-    beautier::get_path("anthus_nd2.fas")
+    babette::get_path("anthus_nd2.fas")
   )
 
   testthat::expect_equal(
     system.file("extdata", "anthus_aco.fas", package = "babette"),
-    beautier::get_path("anthus_aco.fas")
+    babette::get_path("anthus_aco.fas")
   )
 
 })
@@ -17,7 +17,7 @@ test_that("use", {
 test_that("abuse", {
 
   testthat::expect_error(
-    beautier::get_path("abs.ent"),
+    babette::get_path("abs.ent"),
     "'filename' must be the name of a file in 'inst/extdata'"
   )
 

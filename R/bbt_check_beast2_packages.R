ab0b4c01d581121b5aa33340bf43cd3ea8a9bc95
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-02 12:01:22 +0200

    Enable test again

--- R/bbt_check_beast2_packages.R
+++ R/bbt_check_beast2_packages.R
@@ -13,14 +13,14 @@ bbt_check_beast2_packages <- function(
       "Tip: use 'mauricer::install_beast2_pkg(\"NS\")'"
     )
   }
-  # if (beautier::is_mcmc_nested_sampling(mcmc) &&
-  #     !beastier::is_bin_path(beast2_path)
-  # ) {
-  #   stop(
-  #     "When using nested sampling (that is, ",
-  #     "using 'create_mcmc_nested_sampling'), ",
-  #     "one must use the binary BEAST2 executable (that is, using ",
-  #     "'beast2_path = get_default_beast2_bin_path()')"
-  #   )
-  # }
+  if (beautier::is_mcmc_nested_sampling(mcmc) &&
+      !beastier::is_bin_path(beast2_path)
+  ) {
+    stop(
+      "When using nested sampling (that is, ",
+      "using 'create_mcmc_nested_sampling'), ",
+      "one must use the binary BEAST2 executable (that is, using ",
+      "'beast2_path = get_default_beast2_bin_path()')"
+    )
+  }
 }

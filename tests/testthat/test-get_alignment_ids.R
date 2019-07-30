61a8decc0bb2fe96c4ff65b0ffbda4c9383a57ae
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-30 13:15:31 +0200

    Fix test

--- tests/testthat/test-get_alignment_ids.R
+++ tests/testthat/test-get_alignment_ids.R
@@ -2,12 +2,12 @@ context("get_alignment_ids")
 
 test_that("use", {
 
-  testthat::expect_equal(
-    babette:::get_alignment_ids(get_babette_path("2_4.xml")),
+  expect_equal(
+    get_alignment_ids(get_babette_path("2_4.xml")),
     c("test_output_0")
   )
-  testthat::expect_equal(
-    babette:::get_alignment_ids(get_babette_path("anthus_2_4.xml")),
+  expect_equal(
+    get_alignment_ids(get_babette_path("anthus_2_4.xml")),
     c("Anthus_nd2", "Anthus_aco")
   )
 
@@ -15,9 +15,6 @@ test_that("use", {
 
 test_that("use", {
 
-  testthat::expect_error(
-    babette:::get_alignment_ids("abs.ent"),
-    "'xml_filename' must be the name of an existing file"
-  )
+  expect_error(get_alignment_ids("abs.ent"), "File '.*' not found")
 
 })

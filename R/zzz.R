e2385263b8672cc3d1d0208cff586ead4bb568d9
Giappo <glaudanno@gmail.com>
2019-09-13 16:36:06 +0200

    update_babette does not work

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,5 @@
 .onLoad <- function(libname, pkgname) { # nolint .onLoad cannot be snake_case
 
-  update_babette()
-
   suppressPackageStartupMessages(
     lapply(
       c("beautier", "beastier", "mauricer", "tracerer"),

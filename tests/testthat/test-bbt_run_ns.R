b0073ceb8539153689bd76eadb9cdfff0c249d70
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-28 11:34:22 +0100

    Mute verbosity test

--- tests/testthat/test-bbt_run_ns.R
+++ tests/testthat/test-bbt_run_ns.R
@@ -49,54 +49,20 @@ test_that("use, bin (Linux only)", {
   expect_true("ess" %in% names(out$ns))
 })
 
-test_that("use, jar (Windows + ?Linux)", {
+test_that("be verbose (yet muted)", {
 
-  skip("Not now, Issue #74")
-  skip("Issue #68")
   if (!beastier::is_beast2_installed()) return()
   if (rappdirs::app_dir()$os != "win") return()
   if (!mauricer::is_beast2_ns_pkg_installed()) return()
 
-  testit::assert(mauricer::is_beast2_ns_pkg_installed())
-  out <- bbt_run(
+  sink("/dev/null")
+  bbt_run(
     fasta_filename = get_babette_path("anthus_aco.fas"),
-    mcmc = create_mcmc_nested_sampling(
-      chain_length = 1000,
-      store_every = 1000,
-      sub_chain_length = 500
-    ),
+    mcmc = create_test_ns_mcmc(),
     beast2_path = get_default_beast2_jar_path(),
     verbose = TRUE
   )
-
-  expect_true("estimates" %in% names(out))
-  expect_true("anthus_aco_trees" %in% names(out))
-  expect_true("operators" %in% names(out))
-  expect_true("output" %in% names(out))
-  expect_true("Sample" %in% names(out$estimates))
-  expect_true("posterior" %in% names(out$estimates))
-  expect_true("likelihood" %in% names(out$estimates))
-  expect_true("prior" %in% names(out$estimates))
-  expect_true("treeLikelihood" %in% names(out$estimates))
-  expect_true("TreeHeight" %in% names(out$estimates))
-  expect_true("YuleModel" %in% names(out$estimates))
-  expect_true("birthRate" %in% names(out$estimates))
-  expect_true("operator" %in% names(out$operators))
-  expect_true("p" %in% names(out$operators))
-  expect_true("accept" %in% names(out$operators))
-  expect_true("reject" %in% names(out$operators))
-  expect_true("acceptFC" %in% names(out$operators))
-  expect_true("rejectFC" %in% names(out$operators))
-  expect_true("rejectIv" %in% names(out$operators))
-  expect_true("rejectOp" %in% names(out$operators))
-
-  expect_true("ns" %in% names(out))
-  expect_true("marg_log_lik" %in% names(out$ns))
-  expect_true("marg_log_lik_sd" %in% names(out$ns))
-
-  expect_true("estimates" %in% names(out$ns))
-  expect_true("trees" %in% names(out$ns))
-  expect_true("ess" %in% names(out$ns))
+  sink()
 })
 
 test_that("abuse", {

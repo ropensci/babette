25e95eb88d1175be715b14e4c84255fabb0edf75
richelbilderbeek <richel@richelbilderbeek.nl>
2019-12-02 13:19:43 +0100

    'bbt_process_pkg_output' is not exported

--- R/bbt_run_from_model.R
+++ R/bbt_run_from_model.R
@@ -202,7 +202,7 @@ bbt_run_from_model <- function(
 
   # Process the package specific output,
   # for example, add an 'ns' atributed for Nested Sampling
-  babette::bbt_process_pkg_output(
+  bbt_process_pkg_output(
     out = out,
     inference_model = inference_model
   )

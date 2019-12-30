206081198db420c5590ea593036ad26411af75c7
richelbilderbeek <richel@richelbilderbeek.nl>
2019-12-30 14:27:04 +0100

    Improve doc

--- R/babette.R
+++ R/babette.R
@@ -1,7 +1,7 @@
 #' babette: A package for Bayesian phylogenetics.
 #'
 #' 'babette' provides for an alternative workflow of using
-#' the popular phylogenetics tool BEAST2, including
+#' the popular phylogenetics tool 'BEAST2', including
 #' it peripheral tools. From an alignment and
 #' prior selection, a posterior of jointly estimated
 #' phylogenies and parameter estimates is generated.
@@ -10,19 +10,19 @@
 #' These are packages associated with 'beautier':
 #' \itemize{
 #'   \item{
-#'     The package 'beautier' can create
+#'     The package '\link[beautier]{beautier}' can create
 #'     BEAST2 input files from R
 #'   }
 #'   \item{
-#'     The package 'beastier' can run
+#'     The package '\link[beastier]{beastier}' can run
 #'     BEAST2 from R
 #'   }
 #'   \item{
-#'     The package 'mauricer' can install
+#'     The package '\link[mauricer]{mauricer}' can install
 #'     BEAST2 packages from R
 #'   }
 #'   \item{
-#'     The package 'tracerer' can parse
+#'     The package '\link[tracerer]{tracerer}' can parse
 #'     BEAST2 output files from R
 #'   }
 #' }

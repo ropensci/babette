4cdb35c77b4e2ac8e5e518750e3f4ef6b3f0a9ed
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-01 19:07:01 +0100

    Add 'bbt_delete_temp_files', progress #85

---
+++ tests/testthat/test-bbt_delete_temp_files.R
@@ -0,0 +1,31 @@
+test_that("use", {
+
+  inference_model <- create_test_inference_model()
+  beast2_options <- create_beast2_options()
+
+  expect_silent(
+    bbt_run_from_model(
+      fasta_filename = get_fasta_filename(),
+      inference_model = inference_model,
+      beast2_options = beast2_options
+    )
+  )
+  # Temp files created
+  expect_true(file.exists(inference_model$mcmc$tracelog$filename))
+  expect_true(file.exists(inference_model$mcmc$treelog$filename))
+  expect_true(file.exists(inference_model$mcmc$screenlog$filename))
+  expect_true(file.exists(beast2_options$input_filename))
+  expect_true(file.exists(beast2_options$output_state_filename))
+
+  bbt_delete_temp_files(
+    inference_model = inference_model,
+    beast2_options = beast2_options
+  )
+
+  # Temp files created
+  expect_false(file.exists(inference_model$mcmc$tracelog$filename))
+  expect_false(file.exists(inference_model$mcmc$treelog$filename))
+  expect_false(file.exists(inference_model$mcmc$screenlog$filename))
+  expect_false(file.exists(beast2_options$input_filename))
+  expect_false(file.exists(beast2_options$output_state_filename))
+})

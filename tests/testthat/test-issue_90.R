a994b4b6c819622c7b1c707ff623980ce53e7498
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-26 15:38:30 +0200

    Thanks @lintr-bot

--- tests/testthat/test-issue_90.R
+++ tests/testthat/test-issue_90.R
@@ -1,13 +1,20 @@
 test_that("use", {
 
   skip("Issue 90, Issue #90")
-  filename <- get_babette_path("Felinecoronavirus_Envelope_1.fas")
-  tipdates_filename <- get_babette_path("Felinecoronavirus_Envelope_1_no_quotes.txt")
+  filename <- get_babette_path(
+    "Felinecoronavirus_Envelope_1.fas"
+  )
+  tipdates_filename <- get_babette_path(
+    "Felinecoronavirus_Envelope_1_no_quotes.txt"
+  )
   inference_model <- create_inference_model(
     mcmc = create_mcmc(chain_length = 10000),
     tipdates_filename = tipdates_filename,
     site_model = create_jc69_site_model(),
-    clock_model = create_rln_clock_model(mean_clock_rate =  0.001,mean_rate_prior_distr = create_normal_distr()),
+    clock_model = create_rln_clock_model(
+      mean_clock_rate =  0.001,
+      mean_rate_prior_distr = create_normal_distr()
+    ),
     tree_prior = create_cbs_tree_prior()
   )
   check_inference_model(inference_model)
@@ -19,17 +26,18 @@ test_that("use", {
     inference_model = inference_model
   )
   readLines(output_filename)
-  #inference_model$mcmc$tracelog$filename <- gsub(pattern = ".fas",".log",filename)
-  #inference_model$mcmc$treelog$filename <- gsub(pattern = ".fas",".trees",filename)
-
-  out <- bbt_run_from_model(
-    fasta_filename = filename,
-    beast2_options = create_beast2_options(verbose = TRUE) ,
-    inference_model = inference_model
+  inference_model$mcmc$tracelog$filename <- gsub(
+    pattern = ".fas", ".log", filename
+  )
+  inference_model$mcmc$treelog$filename <- gsub(
+    pattern = ".fas", ".trees", filename
   )
 
-  bbt_delete_temp_files(
-    inference_model = inference_model,
-    beast2_options = beast2_options
+  expect_silent(
+    bbt_run_from_model(
+      fasta_filename = filename,
+      beast2_options = create_beast2_options(verbose = TRUE),
+      inference_model = inference_model
+    )
   )
 })

da5710d99651b8ec059ec906822312aa89bc381c
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-26 13:56:02 +0200

    Reproduce bug at #90

---
+++ tests/testthat/test-issue_90.R
@@ -0,0 +1,28 @@
+test_that("use", {
+
+  skip("Issue 90, Issue #90")
+  filename <- get_babette_path("Felinecoronavirus_Envelope_1.fas")
+  tipdates_filename <- get_babette_path("Felinecoronavirus_Envelope_1.txt")
+  inference_model <- create_inference_model(
+    mcmc = create_mcmc(chain_length = 10000),
+    tipdates_filename = tipdates_filename,
+    site_model = create_jc69_site_model(),
+    clock_model = create_rln_clock_model(mean_clock_rate =  0.001,mean_rate_prior_distr = create_normal_distr()),
+    tree_prior = create_cbs_tree_prior()
+  )
+  check_inference_model(inference_model)
+
+  #inference_model$mcmc$tracelog$filename <- gsub(pattern = ".fas",".log",filename)
+  #inference_model$mcmc$treelog$filename <- gsub(pattern = ".fas",".trees",filename)
+
+  out <- bbt_run_from_model(
+    fasta_filename = filename,
+    beast2_options = create_beast2_options(verbose = TRUE) ,
+    inference_model = inference_model
+  )
+
+  bbt_delete_temp_files(
+    inference_model = inference_model,
+    beast2_options = beast2_options
+  )
+})

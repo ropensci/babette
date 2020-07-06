80c0062555939af4393dafd8cfb23516dfbca829
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-06 17:20:42 +0200

    EOD

--- tests/testthat/test-issue_90.R
+++ tests/testthat/test-issue_90.R
@@ -8,30 +8,26 @@ test_that("use", {
     "Felinecoronavirus_Envelope_1_no_quotes.txt"
   )
   inference_model <- create_inference_model(
-    mcmc = create_mcmc(chain_length = 10000),
     tipdates_filename = tipdates_filename,
-    site_model = create_jc69_site_model(),
-    clock_model = create_rln_clock_model(
-      mean_clock_rate =  0.001,
-      mean_rate_prior_distr = create_normal_distr()
-    ),
-    tree_prior = create_cbs_tree_prior()
+    clock_model = create_rln_clock_model()
   )
-  check_inference_model(inference_model)
+  #check_inference_model(inference_model)
 
-  output_filename <- tempfile()
-  beautier::create_beast2_input_file_from_model(
-    input_filename = filename,
-    output_filename = output_filename,
-    inference_model = inference_model
-  )
-  readLines(output_filename)
-  inference_model$mcmc$tracelog$filename <- gsub(
-    pattern = ".fas", ".log", filename
-  )
-  inference_model$mcmc$treelog$filename <- gsub(
-    pattern = ".fas", ".trees", filename
-  )
+  # output_filename <- tempfile()
+  # beautier::create_beast2_input_file_from_model(
+  #   input_filename = filename,
+  #   output_filename = output_filename,
+  #   inference_model = inference_model
+  # )
+
+  # Could not find object associated with idref clockRate.c:Felinecoronavirus_Envelope_1          # nolint
+  #                                                                                               # nolint
+  # Error detected about here:                                                                    # nolint
+  #                                                                                               # nolint
+  # <beast>                                                                                       # nolint
+  #   <run id='mcmc' spec='MCMC'>                                                                 # nolint
+  #     <operator id='StrictClockRateScaler.c:Felinecoronavirus_Envelope_1' spec='ScaleOperator'> # nolint
+  #       <input>                                                                                 # nolint
 
   expect_silent(
     bbt_run_from_model(

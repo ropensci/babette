48935aea3edbd18c8ab2028d8b871276abe2f82c
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-24 13:39:02 +0200

    No testthat tests in example code

--- R/create_test_bbt_run_output.R
+++ R/create_test_bbt_run_output.R
@@ -6,33 +6,7 @@
 #'   or \code{\link{bbt_run_from_model}}
 #' @author Richèl J.C. Bilderbeek
 #' @examples
-#'  out <- create_test_bbt_run_output()
-#'
-#'  library(testthat)
-#'  expect_true("estimates" %in% names(out))
-#'  expect_true("anthus_aco_trees" %in% names(out))
-#'  expect_true("operators" %in% names(out))
-#'  expect_true("output" %in% names(out))
-#'  expect_true(is_phylo(out$anthus_aco_trees[[1]]))
-#'  expect_true(length(out$anthus_aco_trees) == 2)
-#'
-#'  expect_true("Sample" %in% names(out$estimates))
-#'  expect_true("posterior" %in% names(out$estimates))
-#'  expect_true("likelihood" %in% names(out$estimates))
-#'  expect_true("prior" %in% names(out$estimates))
-#'  expect_true("treeLikelihood" %in% names(out$estimates))
-#'  expect_true("TreeHeight" %in% names(out$estimates))
-#'  expect_true("YuleModel" %in% names(out$estimates))
-#'  expect_true("birthRate" %in% names(out$estimates))
-#'
-#'  expect_true("operator" %in% names(out$operators))
-#'  expect_true("p" %in% names(out$operators))
-#'  expect_true("accept" %in% names(out$operators))
-#'  expect_true("reject" %in% names(out$operators))
-#'  expect_true("acceptFC" %in% names(out$operators))
-#'  expect_true("rejectFC" %in% names(out$operators))
-#'  expect_true("rejectIv" %in% names(out$operators))
-#'  expect_true("rejectOp" %in% names(out$operators))
+#' create_test_bbt_run_output()
 #' @export
 create_test_bbt_run_output <- function() {
 

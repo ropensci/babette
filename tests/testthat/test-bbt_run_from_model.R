9467066b848037a6b6dfd4a3dea76f090c9492b1
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-23 11:32:52 +0200

    Use rappdirs instead of tempfile for Travis CI

--- tests/testthat/test-bbt_run_from_model.R
+++ tests/testthat/test-bbt_run_from_model.R
@@ -141,7 +141,11 @@ test_that("use, nested sampling, in custom folder", {
   if (!beastier::is_on_ci()) return()
   if (!curl::has_internet()) return()
 
-  beast2_folder <- tempfile(pattern = "babette_")
+  # Cannot use tempfile on Travis CI
+  beast2_folder <- file.path(
+    rappdirs::user_cache_dir(),
+    basename(tempfile(pattern = "babette_"))
+  )
   mcbette_state <- mcbette::get_mcbette_state(beast2_folder = beast2_folder)
   mcbette_state$beast2_installed <- TRUE
   mcbette_state$ns_installed <- TRUE
@@ -149,12 +153,16 @@ test_that("use, nested sampling, in custom folder", {
     mcbette_state = mcbette_state,
     beast2_folder = beast2_folder
   )
-  expect_true(mauricer::is_beast2_ns_pkg_installed(beast2_folder = beast2_folder))
-  bbt_run_from_model(
-    fasta_filename = get_babette_path("anthus_aco.fas"),
-    inference_model = beautier::create_test_ns_inference_model(),
-    beast2_options = create_beast2_options(
-      beast2_path = get_default_beast2_bin_path(beast2_folder = beast2_folder)
+  expect_true(mauricer::is_beast2_ns_pkg_installed(
+    beast2_folder = beast2_folder)
+  )
+  expect_silent(
+    bbt_run_from_model(
+      fasta_filename = get_babette_path("anthus_aco.fas"),
+      inference_model = beautier::create_test_ns_inference_model(),
+      beast2_options = create_beast2_options(
+        beast2_path = get_default_beast2_bin_path(beast2_folder = beast2_folder)
+      )
     )
   )
 })

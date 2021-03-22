1e0293bd414f564c65d39b030335891730fc032f
richelbilderbeek <richel@richelbilderbeek.nl>
2021-03-22 15:10:54 +0100

    Expose #93 and #94

--- tests/testthat/test-issues.R
+++ tests/testthat/test-issues.R
@@ -29,3 +29,48 @@ test_that("use", {
     )
   )
 })
+
+test_that("clockRate.c ID and ClockPrior.c ID added twice", {
+
+  skip("Issue 93, Issue #93")
+
+  if (!is_beast2_installed()) return()
+  # From https://github.com/ropensci/beautier/issues/127
+  # From https://github.com/ropensci/beautier/issues/128
+
+  # clock_model
+  clock_rate <- 0.0000001
+  clock_model <- create_strict_clock_model(
+    clock_rate_param = create_clock_rate_param(value = clock_rate),
+    clock_rate_distr = create_log_normal_distr(
+      value = clock_rate,
+      m = 1,
+      s = 1.25
+    )
+  )
+
+  # MRCA PRIOR
+  mrca_prior <- create_mrca_prior(
+    is_monophyletic = TRUE,
+    mrca_distr = create_laplace_distr(mu = 1990)
+  )
+
+
+  inference_model <- create_inference_model(
+    clock_model = clock_model,
+    mrca_prior = mrca_prior,
+    tipdates_filename = beautier::get_beautier_path(
+      "THAILAND_TEST.clust_1.dated.txt"
+    ),
+    mcmc = create_test_mcmc()
+  )
+  expect_error(
+    bbt_run_from_model(
+      fasta_filename = beautier::get_beautier_path(
+        "THAILAND_TEST.clust_1.dated.fa"
+      ),
+      inference_model = inference_model
+    ),
+    "Two sequences with different length found"
+  )
+})

91c98456be8706567d803ec72f3de879fe523949
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-20 10:48:27 +0200

    Make sure no temp files are left undeleted

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,13 @@
 library(testthat)
 library(babette)
 
+# Make sure no temp files are left undeleted
+beautier::clear_beautier_cache()
+beautier::check_empty_beautier_folder()
+beastier::clear_beastier_cache()
+beastier::check_empty_beastier_folder()
+
 test_check("babette")
 
-testthat::expect_equal(
-  0,
-  length(list.files(rappdirs::user_cache_dir(appname = "beautier")))
-)
-
-testthat::expect_equal(
-  0,
-  length(list.files(rappdirs::user_cache_dir(appname = "beastier")))
-)
+beautier::check_empty_beautier_folder()
+beastier::check_empty_beastier_folder()

496380183486f659c8017709f4fa444b32aff649
richelbilderbeek <richel@richelbilderbeek.nl>
2021-09-12 18:21:15 +0200

    Remove temp folder after tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -10,3 +10,7 @@ test_check("babette")
 beautier::check_empty_beautier_folder()
 beastier::check_empty_beastier_folder()
 # beastierinstall::clear_beautier_cache() ; beastierinstall::clear_beastier_cache() # nolint
+unlink(
+  dirname(beastier::get_beastier_tempfilename()),
+  recursive = TRUE
+)

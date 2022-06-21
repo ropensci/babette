25a66789f182bd13b38c68c68173f8f27e4494ff
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-21 08:40:19 +0200

    Import each function by name

--- scripts/create_imports.R
+++ scripts/create_imports.R
@@ -1,19 +1,39 @@
-
+###############################################################################
+# beautier
+###############################################################################
 library(beautier)
+
+pattern <- "^(add|are|beastier_report|calc|check|compare|continue|count|create|extract|fasta|find|get|gives|has|indent|install|is|parse|print|remove|rename|run|save|unindent|uninstall)" # nolint indeed a long line
 
 all_beautier_function_names <- ls("package:beautier")
 beautier_function_names <- stringr::str_subset(
   all_beautier_function_names,
-  "^(are|check|compare|count|create|extract|fasta|find|get|has|indent|is|remove|rename|unindent)" # nolint indeed a long line
+  pattern = pattern
 )
-beautier_text <- paste("#' @importFrom beautier",beautier_function_names)
+beautier_text <- paste("#' @importFrom beautier", beautier_function_names)
 beautier_text
 
+###############################################################################
+# tracerer
+###############################################################################
+library(tracerer)
+
+all_tracerer_function_names <- ls("package:tracerer")
+tracerer_function_names <- stringr::str_subset(
+  all_tracerer_function_names,
+  pattern = pattern
+)
+tracerer_text <- paste("#' @importFrom tracerer", tracerer_function_names)
+tracerer_text
+
+###############################################################################
+# beastier
+###############################################################################
 library(beastier)
 all_beastier_function_names <- ls("package:beastier")
 beastier_function_names <- stringr::str_subset(
   all_beastier_function_names,
-  "^(add|are|beastier_report|check|continue|create|extract|get|gives|has|install|is|print|remove|rename|run|save|uninstall)" # nolint indeed a long line
+  pattern = pattern
 )
 beastier_function_names <- stringr::str_subset(
   beastier_function_names,
@@ -23,5 +43,21 @@ beastier_function_names <- stringr::str_subset(
 beastier_text <- paste("#' @importFrom beastier", beastier_function_names)
 beastier_text
 
-texts <- c(beautier_text, beastier_text)
+###############################################################################
+# mauricer
+###############################################################################
+library(mauricer)
+all_mauricer_function_names <- ls("package:mauricer")
+mauricer_function_names <- stringr::str_subset(
+  all_mauricer_function_names,
+  pattern = pattern
+)
+mauricer_text <- paste("#' @importFrom mauricer", mauricer_function_names)
+mauricer_text
+
+
+###############################################################################
+# combine
+###############################################################################
+texts <- c(beautier_text, tracerer_text, beastier_text, mauricer_text)
 writeLines(texts, con = "~/import.txt")

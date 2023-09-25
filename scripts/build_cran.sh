2b8e1de5333db28cffbc904eb8e152f531bba3c1
richelbilderbeek <richel@richelbilderbeek.nl>
2023-09-25 17:44:48 +0200

    Fix bash script

--- scripts/build_cran.sh
+++ scripts/build_cran.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-#
-# Check if the package builds cleanly under CRAN guidelines
-#
-# Usage:
-#
-#   ./build_cran
-#
+
 rm -rf build
 mkdir build
 
-cp -r ../R build
 cp -r ../inst build
 cp -r ../man build
+# cp -r ../Meta build
+cp -r ../R build
+# cp -r ../src build
 cp -r ../vignettes build
 cp ../DESCRIPTION build
 cp ../NAMESPACE build
 
-cd build
+cd build || exit 42
 R CMD build .
-R CMD check --as-cran $(ls *.tar.gz)
+gz_files=$(ls ./*.tar.gz)
+R CMD check --as-cran "${gz_files}"
